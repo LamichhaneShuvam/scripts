058d096650aacc5b1b0137d1823e7fed7c658d91
shuvam lamichhane <shuvamlamichhane@gmail.com>
2025-10-19 12:20:28 +0545

    Update: 1. Docker installation script updated according to the latest docker docs

--- ubuntu/setup_docker.sh
+++ ubuntu/setup_docker.sh
@@ -4,36 +4,39 @@
 
 # Function to install Docker
 install_docker() {
-    echo "Installing Docker..."
+  echo "Removing conflicting packages"
+  for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt remove $pkg -y; done
 
-    # Add Docker's official GPG key:
-    sudo apt update -y
-    sudo apt install ca-certificates curl -y
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
+  echo "Installing Docker..."
 
-    # Add the repository to Apt sources:
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  # Add Docker's official GPG key:
+  sudo apt update -y
+  sudo apt install ca-certificates curl -y
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-    sudo apt update -y
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-    # Install docker
-    sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+  sudo apt update -y
 
-    # Start the docker service
-    sudo systemctl start docker
+  # Install docker
+  sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-    # Enable docker to start on boot
-    sudo systemctl enable docker
+  # Start the docker service
+  sudo systemctl start docker
 
-    # Add current user to docker group to run Docker commands without sudo
-    sudo usermod -aG docker $USER
+  # Enable docker to start on boot
+  sudo systemctl enable docker
 
-    echo "Docker installed. You may need to log out and back in for Docker to work without sudo."
+  # Add current user to docker group to run Docker commands without sudo
+  sudo usermod -aG docker $USER
+
+  echo "Docker installed. You may need to log out and back in for Docker to work without sudo."
 }
 
 # Main script
